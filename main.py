808278ec48990dd51b918b26de86ceda2493e501
shreekant-lohagale <shreekantlohagale@gmail.com>
2026-05-29 07:23:34 +0530

    Add initial implementation of ChatMistralAI model and create test file

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
 
 load_dotenv()
 
- 
+model = ChatMistralAI(model = "mistral-small-2506")
+result = model.invoke("Hello, how are you?")
+print(result.content)
