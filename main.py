9330e5ea4e677b2e29a1c9187618b0d9e31b542a
shreekant-lohagale <shreekantlohagale@gmail.com>
2026-05-30 08:13:04 +0530

    Add text loading and summarization functionality using ChatPromptTemplate

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
+from langchain_community.document_loaders import TextLoader
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
+data = TextLoader("D:/Gen AI-project1/document loaders/txt.txt")
+docs = data.load()
+
+template = ChatPromptTemplate.from_messages(
+    [("system", "You are a AI that summarizes the following text."),
+     ("human", "{data}")]
+
+)
+
 model = ChatMistralAI(model = "mistral-small-2506")
-result = model.invoke("Hello, how are you?")
+
+prompt = template.format_messages(data = docs[0].page_content)
+result = model.invoke(prompt)
 print(result.content)
