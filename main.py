292aa2576850c2d092a4720ff6bcf85df0eaf9e1
shreekant-lohagale <shreekantlohagale@gmail.com>
2026-05-31 07:05:57 +0530

    Refactor document loading to use PyPDFLoader and add WebBaseLoader for web content

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
 from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
-data = TextLoader("D:/Gen AI-project1/document loaders/txt.txt")
+data = PyPDFLoader("D:/Gen AI-project1/document loaders/Third Year -AIML_Syllabus.pdf")
 docs = data.load()
 
 template = ChatPromptTemplate.from_messages(
@@ -16,6 +17,6 @@ template = ChatPromptTemplate.from_messages(
 
 model = ChatMistralAI(model = "mistral-small-2506")
 
-prompt = template.format_messages(data = docs[0].page_content)
+prompt = template.format_messages(data = docs)
 result = model.invoke(prompt)
 print(result.content)
