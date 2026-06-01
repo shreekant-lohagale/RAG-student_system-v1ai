97f2d58468bc87dc2d3e1d7e79c83500178f9e82
shreekant-lohagale <shreekantlohagale@gmail.com>
2026-06-01 07:51:07 +0530

    Add text splitter implementation for document processing

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from langchain_mistralai import ChatMistralAI
 from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
 data = PyPDFLoader("D:/Gen AI-project1/document loaders/Third Year -AIML_Syllabus.pdf")
 docs = data.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+chunks = splitter.split_documents(docs)
+
 
 template = ChatPromptTemplate.from_messages(
     [("system", "You are a AI that summarizes the following text."),
