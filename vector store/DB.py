bbbfd56bcaf1cb65f57c526d17e53e420eb4e68f
shreekant-lohagale <shreekantlohagale@gmail.com>
2026-06-03 07:35:50 +0530

    Refactor import statement and improve code formatting in DB.py

--- vector store/DB.py
+++ vector store/DB.py
@@ -1,5 +1,5 @@
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.documents import Document
 
 embedding_model = HuggingFaceEmbeddings(
@@ -7,9 +7,26 @@ embedding_model = HuggingFaceEmbeddings(
 )
 
 docs = [
-    Document(page_content="Python is widely used for data science.", metadata={"source": "doc1"}),
-    Document(page_content="JavaScript is popular for web development.", metadata={"source": "doc2"})
+    Document(
+        page_content="Python is widely used for data science.",
+        metadata={"source": "doc1"}
+    ),
+    Document(
+        page_content="JavaScript is popular for web development.",
+        metadata={"source": "doc2"}
+    )
 ]
 
-vectorstore = Chroma.from_documents(documents = docs, embedding=embedding_model, persist_directory="./chroma_db")
+vectorstore = Chroma.from_documents(
+    documents=docs,
+    embedding=embedding_model,
+    persist_directory="./chroma_db"
+)
 
+results = vectorstore.similarity_search(
+    "What programming language is used for data science?",
+    k=2
+)
+
+for doc in results:
+    print(doc.page_content)
